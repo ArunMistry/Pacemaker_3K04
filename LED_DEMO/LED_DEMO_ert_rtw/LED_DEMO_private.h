83c2c3b7bc84aa983c88f40c4c7f682c96f35938
Arun Mistry <mistra11@mcmaster.ca>
2021-10-04 09:29:10 -0400

    LED FOLDER

---
+++ LED_DEMO/LED_DEMO_ert_rtw/LED_DEMO_private.h
@@ -0,0 +1,29 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * File: LED_DEMO_private.h
+ *
+ * Code generated for Simulink model 'LED_DEMO'.
+ *
+ * Model version                  : 1.3
+ * Simulink Coder version         : 9.5 (R2021a) 14-Nov-2020
+ * C/C++ source code generated on : Mon Oct  4 09:12:56 2021
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: ARM Compatible->ARM Cortex
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_LED_DEMO_private_h_
+#define RTW_HEADER_LED_DEMO_private_h_
+#include "rtwtypes.h"
+#endif                                 /* RTW_HEADER_LED_DEMO_private_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
---
+++ LED_DEMO/LED_DEMO_ert_rtw/ert_main.c
@@ -0,0 +1,117 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * File: ert_main.c
+ *
+ * Code generated for Simulink model 'LED_DEMO'.
+ *
+ * Model version                  : 1.3
+ * Simulink Coder version         : 9.5 (R2021a) 14-Nov-2020
+ * C/C++ source code generated on : Mon Oct  4 09:12:56 2021
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: ARM Compatible->ARM Cortex
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#include "LED_DEMO.h"
+#include "LED_DEMO_private.h"
+#include "rtwtypes.h"
+#include "limits.h"
+#include "board.h"
+#include "mw_cmsis_rtos.h"
+#define UNUSED(x)                      x = x
+#define NAMELEN                        16
+
+/* Function prototype declaration*/
+void exitFcn(int sig);
+void *terminateTask(void *arg);
+void *baseRateTask(void *arg);
+void *subrateTask(void *arg);
+volatile boolean_T stopRequested = false;
+volatile boolean_T runModel = true;
+mw_signal_event_t stopSem;
+mw_signal_event_t baserateTaskSem;
+mw_thread_t schedulerThread;
+mw_thread_t baseRateThread;
+void *threadJoinStatus;
+int terminatingmodel = 0;
+void *baseRateTask(void *arg)
+{
+  runModel = (rtmGetErrorStatus(LED_DEMO_M) == (NULL));
+  while (runModel) {
+    mw_osSignalEventWaitEver(&baserateTaskSem);
+    LED_DEMO_step();
+
+    /* Get model outputs here */
+    stopRequested = !((rtmGetErrorStatus(LED_DEMO_M) == (NULL)));
+  }
+
+  runModel = 0;
+  terminateTask(arg);
+  mw_osThreadExit((void *)0);
+  return NULL;
+}
+
+void exitFcn(int sig)
+{
+  UNUSED(sig);
+  rtmSetErrorStatus(LED_DEMO_M, "stopping the model");
+  runModel = 0;
+}
+
+void *terminateTask(void *arg)
+{
+  UNUSED(arg);
+  terminatingmodel = 1;
+
+  {
+    runModel = 0;
+  }
+
+  /* Disable rt_OneStep() here */
+
+  /* Terminate model */
+  LED_DEMO_terminate();
+  mw_osSignalEventRelease(&stopSem);
+  return NULL;
+}
+
+int main(int argc, char **argv)
+{
+  SystemCoreClockUpdate();
+  hardware_init();
+  rtmSetErrorStatus(LED_DEMO_M, 0);
+
+  /* Initialize model */
+  LED_DEMO_initialize();
+
+  /* Call RTOS Initialization function */
+  mw_RTOSInit(0.001, 0);
+
+  /* Wait for stop semaphore */
+  mw_osSignalEventWaitEver(&stopSem);
+
+#if (MW_NUMBER_TIMER_DRIVEN_TASKS > 0)
+
+  {
+    int i;
+    for (i=0; i < MW_NUMBER_TIMER_DRIVEN_TASKS; i++) {
+      CHECK_STATUS(mw_osSignalEventDelete(&timerTaskSem[i]), 0,
+                   "mw_osSignalEventDelete");
+    }
+  }
+
+#endif
+
+  return 0;
+}
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
